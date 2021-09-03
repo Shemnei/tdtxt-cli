289f3ff297fbc170bb1505633d6c31646ec93761
Jonas Grawe <Shemnei@users.noreply.github.com>
2021-09-03 18:34:07 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Rust project template
+
+## Setup (Clone)
+
+1) Reset the repository
+
+```bash
+rm -rf .git
+git init
+```
+2) Init cargo project
+
+```bash
+cargo init --lib
+# OR
+cargo init --bin
+```
+
+3) If it is a binary, create `src/main.rs`
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+#![allow(rustdoc::private_intra_doc_links)]
+#![deny(
+    // Documentation
+	// TODO: rustdoc::broken_intra_doc_links,
+	// TODO: rustdoc::missing_crate_level_docs,
+	// TODO: missing_docs,
+	// TODO: clippy::missing_docs_in_private_items,
+
+    // Other
+	deprecated_in_future,
+	exported_private_dependencies,
+	future_incompatible,
+	missing_copy_implementations,
+	missing_debug_implementations,
+	private_in_public,
+	rust_2018_compatibility,
+	rust_2018_idioms,
+	trivial_casts,
+	trivial_numeric_casts,
+	unsafe_code,
+	unstable_features,
+	unused_import_braces,
+	unused_qualifications,
+
+	// clippy attributes
+	clippy::missing_const_for_fn,
+	clippy::redundant_pub_crate,
+	clippy::use_self
+)]
+#![cfg_attr(docsrs, feature(doc_cfg), feature(doc_alias))]
